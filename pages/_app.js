ae390c339ac438551e156fe38671830910ac1c67
Ercan <kadri503394@gmail.com>
2022-11-24 02:49:03 +0300

    feat: added new design and sass

--- pages/_app.js
+++ pages/_app.js
@@ -1,6 +1,6 @@
 import "slick-carousel/slick/slick.css";
 import "slick-carousel/slick/slick-theme.css";
-import '../styles/globals.css'
+import '../styles/globals.scss'
 import { Layout } from "../layout/Layout";
 
 function MyApp({ Component, pageProps }) {
